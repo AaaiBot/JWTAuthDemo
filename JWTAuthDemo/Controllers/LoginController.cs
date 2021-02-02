6fc2ebcd4405a55c1b4b2a9b7e5bfdfbf29225a3
AaaiBot <50262079+AaaiBot@users.noreply.github.com>
2021-02-02 14:55:37 +0000

    Add project files.

---
+++ JWTAuthDemo/Controllers/LoginController.cs
@@ -0,0 +1,103 @@
+﻿using JWTAuthDemo.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTAuthDemo.Controllers
+{
+    // todo - seperate into 2x - an authorisation server (identity), and a protected resource server (everyday api)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration config;
+
+        public LoginController(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        public IActionResult Login(string username, string password)
+        {
+            var login = new UserModel
+            {
+                UserName = username,
+                Password = password
+            };
+
+            IActionResult response = Unauthorized();
+
+            var user = AuthenticateUser(login);
+
+            if (user != null)
+            {
+                var tokenStr = GenerateJsonWebToken(user);
+                response = Ok(new { token = tokenStr });
+            }
+
+            return response;
+        }
+
+        [Authorize]
+        [HttpPost("Post")]
+        public string Post()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            var claim = identity.Claims.ToList();
+            var username = claim[0].Value;
+            return $"Welcome to: {username}";
+        }
+
+        [Authorize]
+        [HttpGet("GetValues")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Valule1", "Value2", "Value3" };
+        }
+
+        private string GenerateJsonWebToken(UserModel userModel)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userModel.UserName),
+                new Claim(JwtRegisteredClaimNames.Email, userModel.EmailAddress),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: this.config["Jwt:Issuer"],
+                audience: this.config["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials);
+
+            var encodedToken = new JwtSecurityTokenHandler().WriteToken(token);
+            return encodedToken;
+        }
+
+        private UserModel AuthenticateUser(UserModel login)
+        {
+            // Simplified for demo
+            UserModel user = null;
+            if (login.UserName == "richard" && login.Password == "123")
+            {
+                user = new UserModel { UserName = "Richard", EmailAddress = "richard@abc.com" };
+            }
+
+            return user;
+        }
+    }
+}
+
